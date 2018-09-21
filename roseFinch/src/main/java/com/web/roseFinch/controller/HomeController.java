62cdf2dbe084692ea16d752a460daee7e5f4c2c0
yjj <45am.me@gmail.com>
2018-09-22 03:50:01 +0900

    메인 컨트롤러

--- roseFinch/src/main/java/com/web/roseFinch/controller/HomeController.java
+++ roseFinch/src/main/java/com/web/roseFinch/controller/HomeController.java
@@ -4,17 +4,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
+	
+	// jj >> 쇼핑몰 메인 페이지  
+	@GetMapping(value = "main")
+	public String main() {
+		return "/main";
+	}
+	
+	
 	@RequestMapping(value = "/topSellers")
 	public String home(Model model) {
 		return "topSellers";
 	}
 
+	
 }
