d659acf82cf3046313ca8285f062b4427fe5713a
yjj <45am.me@gmail.com>
2018-10-01 00:37:17 +0900

    common js

--- roseFinch/src/main/webapp/resources/js/common.js
+++ roseFinch/src/main/webapp/resources/js/common.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 	
 	
-	
+	// 하단 - 맨위로 버튼
+	var btnTop = $('#btnTop');
+	btnTop.hide();
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 150) {
+			btnTop.fadeIn();
+		} else {
+			btnTop.fadeOut();
+		}
+	});
+    btnTop.click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500);
+		return false;
+    });
 });
