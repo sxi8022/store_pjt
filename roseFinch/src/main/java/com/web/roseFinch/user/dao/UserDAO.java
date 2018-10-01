22f5de2df6196debcfb709143256ddce0c745322
하서 <38983119+wkdlem66@users.noreply.github.com>
2018-10-01 20:35:11 +0900

    user 로그인/회원가입
    user 로그인/회원가입 추가

---
+++ roseFinch/src/main/java/com/web/roseFinch/user/dao/UserDAO.java
@@ -0,0 +1,18 @@
+package com.web.roseFinch.user.dao;
+
+import com.web.roseFinch.user.vo.UserVO;
+
+public interface UserDAO {
+
+	//유저 회원가입
+		public void insertUser(UserVO vo);
+		//유저 회원수정
+		public void updateUser(UserVO vo);
+		//유저 회원탈퇴
+		public void deleteMember(UserVO vo);
+		//유저 로그인
+		public UserVO selectUser(UserVO vo);
+		//유저 아이디 찾기
+		public UserVO getUser(UserVO vo);
+	
+}
