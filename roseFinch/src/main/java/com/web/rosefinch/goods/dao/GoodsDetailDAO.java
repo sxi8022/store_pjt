591cc28eb1c3ec53a2c30ab06b7806682c365b12
smith <wndmstla123@gmail.com>
2018-10-20 19:07:22 +0900

    상품 상세 dao

---
+++ roseFinch/src/main/java/com/web/rosefinch/goods/dao/GoodsDetailDAO.java
@@ -0,0 +1,21 @@
+package com.web.rosefinch.goods.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.web.rosefinch.goods.vo.GoodsDetailVO;
+import com.web.rosefinch.goods.vo.GoodsImagesVO;
+import com.web.rosefinch.goods.vo.GoodsOptVO;
+import com.web.rosefinch.goods.vo.GoodsReviewVO;
+import com.web.rosefinch.goods.vo.SellerVO;
+
+public interface GoodsDetailDAO {
+	
+	public GoodsDetailVO readGood(int gdsCode);
+	public List<GoodsImagesVO> readGoodImg(int gdsCode);
+	public List<GoodsOptVO> readGoodOpts(int gdsCode);
+	public List<GoodsReviewVO> readGoodReviews(int gdsCode);
+	public List<String> readGoodReviewImgs(int revCode);
+	public Map<String, Double> readCountAvgRating(int gdsCode);
+	public SellerVO readSeller(int selCode);
+}
---
+++ roseFinch/src/main/java/com/web/rosefinch/goods/dao/GoodsDetailDAOImpl.java
@@ -0,0 +1,60 @@
+package com.web.rosefinch.goods.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.web.rosefinch.goods.vo.GoodsDetailVO;
+import com.web.rosefinch.goods.vo.GoodsImagesVO;
+import com.web.rosefinch.goods.vo.GoodsOptVO;
+import com.web.rosefinch.goods.vo.GoodsReviewVO;
+import com.web.rosefinch.goods.vo.SellerVO;
+
+@Repository
+public class GoodsDetailDAOImpl implements GoodsDetailDAO {
+	
+	@Autowired
+	private SqlSession sess;
+
+	private final String namespace = "com.web.rosefinch.mapper.GoodDetailMapper";
+	
+	@Override
+	public GoodsDetailVO readGood(int gdsCode) {
+		return sess.selectOne(namespace+".readGood", gdsCode);
+	}
+
+	@Override
+	public List<GoodsImagesVO> readGoodImg(int gdsCode) {
+		return sess.selectList(namespace+".readGoodImg", gdsCode);
+	}
+
+	@Override
+	public List<GoodsOptVO> readGoodOpts(int gdsCode) {
+		return sess.selectList(namespace+".readGoodOpts", gdsCode);
+	}
+
+	@Override
+	public List<GoodsReviewVO> readGoodReviews(int gdsCode) {
+		return sess.selectList(namespace+".readGoodReviews", gdsCode);
+	}
+	
+	@Override
+	public List<String> readGoodReviewImgs(int revCode) {
+		return sess.selectList(namespace+".readGoodReviewImgs", revCode);
+	}
+
+	@Override
+	public Map<String, Double> readCountAvgRating(int gdsCode) {
+		return sess.selectOne(namespace+".readCountAvgRating", gdsCode);
+		
+	}
+
+	@Override
+	public SellerVO readSeller(int selCode) {
+		return sess.selectOne(namespace+".readSeller", selCode);
+	}
+
+}
