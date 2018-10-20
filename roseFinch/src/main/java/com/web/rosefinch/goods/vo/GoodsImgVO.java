ce429228fdd79ca36b03f89fcf260054cb25cdc3
smith <wndmstla123@gmail.com>
2018-10-20 19:04:19 +0900

    상품 이미지 VO 추가

--- roseFinch/src/main/java/com/web/rosefinch/goods/vo/GoodsImgVO.java
+++ roseFinch/src/main/java/com/web/rosefinch/goods/vo/GoodsImgVO.java
@@ -1,8 +1,45 @@
 package com.web.rosefinch.goods.vo;
 
 public class GoodsImgVO {
-
+	private int gdsImgCode;
+	private int gdsCode;
+	private String gdsImgName;
+	private String gdsImgUrl;
+	private int gdsImgSeq;
 	
-	
-	
+	public int getGdsImgCode() {
+		return gdsImgCode;
+	}
+	public void setGdsImgCode(int gdsImgCode) {
+		this.gdsImgCode = gdsImgCode;
+	}
+	public int getGdsCode() {
+		return gdsCode;
+	}
+	public void setGdsCode(int gdsCode) {
+		this.gdsCode = gdsCode;
+	}
+	public String getGdsImgName() {
+		return gdsImgName;
+	}
+	public void setGdsImgName(String gdsImgName) {
+		this.gdsImgName = gdsImgName;
+	}
+	public String getGdsImgUrl() {
+		return gdsImgUrl;
+	}
+	public void setGdsImgUrl(String gdsImgUrl) {
+		this.gdsImgUrl = gdsImgUrl;
+	}
+	public int getGdsImgSeq() {
+		return gdsImgSeq;
+	}
+	public void setGdsImgSeq(int gdsImgSeq) {
+		this.gdsImgSeq = gdsImgSeq;
+	}
+	@Override
+	public String toString() {
+		return "GoodsImgVO [gdsImgCode=" + gdsImgCode + ", gdsCode=" + gdsCode + ", gdsImgName=" + gdsImgName
+				+ ", gdsImgUrl=" + gdsImgUrl + ", gdsImgSeq=" + gdsImgSeq + "]";
+	}
 }
